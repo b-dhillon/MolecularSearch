081613e7b9a064dcc3a8e683bc19ab15b6b48ea1
B. Dhillon <88413313+b-dhillon@users.noreply.github.com>
2023-01-05 11:29:04 -0800

    begin writing cached data table

---
+++ data.js
@@ -0,0 +1,40 @@
+const data = {
+    "PC_Compounds": [
+        {
+            "id": {
+                "id": {
+                    "cid": 2244
+                }
+            },
+            "props": [
+                {
+                    "urn": {
+                        "label": "Molecular Formula"
+                    },
+                    "value": {
+                        "sval": "C16H19N3O5S"
+                    }
+                },
+                {
+                    "urn": {
+                        "label": "Molecular Weight"
+                    },
+                    "value": {
+                        "fval": 365.44
+                    }
+                },
+            ],
+        },
+    ],
+    "PropertyTable": {
+        "Properties": [
+            {
+                "CID": 2244,
+                "MolecularFormula": "C16H19N3O5S",
+                "MolecularWeight": 365.44,
+                "IUPACName": "penicillin G",
+                "Title": "Penicillin G"
+            }
+        ]
+    }
+}
